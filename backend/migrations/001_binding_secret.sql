c28612f761227f017b0c0bebe1498da3c6007aec
VainRoblox <boxedbaconbot@gmail.com>
2026-08-14 18:33:04 +0200

    vain: update

--- backend/migrations/001_binding_secret.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE bindings ADD COLUMN binding_secret TEXT;
-
-CREATE TABLE client_identities (
-	roblox_userid INTEGER PRIMARY KEY,
-	poll_key TEXT NOT NULL,
-	first_seen INTEGER NOT NULL
-);
