94a0e03f0df83cf90cde1dd457841b711ae7a6ab
VainRoblox <boxedbaconbot@gmail.com>
2026-08-17 19:30:46 +0200

    vain: update

--- src/games/bedwars/6872274481 - game/Blatant/LongJump.lua
+++ src/games/bedwars/6872274481 - game/Blatant/LongJump.lua
@@ -189,7 +189,7 @@ LongJump = vain.Categories.Blatant:CreateModule({
 				end
 			end))
 
-			if store.hand and LongJumpMethods[store.hand.tool.Name] then
+			if store.hand and store.hand.tool and LongJumpMethods[store.hand.tool.Name] then
 				task.spawn(LongJumpMethods[store.hand.tool.Name], getItem(store.hand.tool.Name), start, (CameraDir.Enabled and gameCamera or entitylib.character.RootPart).CFrame.LookVector)
 				return
 			end
